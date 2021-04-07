bf5637fdee2e25f0b415e02a922a0e88f6199e61
ClaireGuerin <guerin.claire01@gmail.com>
2021-04-07 11:24:33 +0200

    defined storyline for funcitonal test. changed first test, first fail

--- test/functional_test.py
+++ test/functional_test.py
@@ -1,6 +1,25 @@
 import pytest
+import os
 
 class TestSimpleRun(object):
 
-	def test_there_is_a_grid(self):
-		assert False, "write this test!"
+	# The user gives a number of parameters necessary to run the simulation
+	def test_there_is_a_parameter_file(self):
+		self.filesListRoot = os.listdir("../")
+		assert "parameters.txt" in self.filesListRoot, "parameter file missing in root"
+
+	# A grid is created
+
+	# The grid is a square of the size given in parameters
+
+	# A population of prey individuals is created, of the size given in parameters
+
+	# The user launches the simulation
+
+	# The simulation runs for the number of generations given in paramaters
+
+	# An output file is created with information on vigilance level in the population
+
+	# The output file contains exactly the number of lines that corresponds to the number of generations
+
+	# A message is generated, informing the user that the simulation has successfully completed.
