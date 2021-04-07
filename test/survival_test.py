d7f1bde899a017fe14f189e5d7743831dd672c68
ClaireGuerin <guerin.claire01@gmail.com>
2021-04-07 18:13:06 +0200

    survival test

--- test/survival_test.py
+++ test/survival_test.py
@@ -6,3 +6,10 @@ class TestSurvivalFeature(object):
 	def test_individuals_have_survival_function(self):
 		assert hasattr(Ind(), "survive"), "individuals cannot survive"
 		assert callable(getattr(Ind(), "survive"))
+
+	def test_individuals_can_die(self):
+		self.ind = Ind()
+		self.ind.survive()
+		assert hasattr(self.ind, "alive")
+		assert self.ind.alive is not None
+		assert type(self.ind.alive) is bool
