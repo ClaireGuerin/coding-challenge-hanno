852680b399339ab279cef6212a10894af5d0e879
ClaireGuerin <guerin.claire01@gmail.com>
2021-04-08 12:02:54 +0200

    resources sharing grid of correct format, but still wrong values (empty)

--- test/grid_test.py
+++ test/grid_test.py
@@ -14,3 +14,15 @@ class TestGridObject(object):
 	def test_cell_resources_correct_format(self):
 		self.grid = Grid(dim=10)
 		assert self.grid.resources.dtype == np.float32
+
+	def test_grid_has_cell_resource_sharing_info(self):
+		self.dimension = 10
+		self.grid = Grid(dim=self.dimension)
+		assert hasattr(self.grid, "share")
+		assert type(self.grid.share) is np.ndarray
+		assert self.grid.share.shape == (self.dimension, self.dimension)
+
+	def test_cell_resource_sharing_correct_format(self):
+		self.grid = Grid(dim=10)
+		assert self.grid.share.dtype == np.float32
+
